a7de98505420c2d6cf3845c0a22e188198d3395f
Huub <huubhoofs@gmail.com>
2020-12-22 20:47:22 +0100

    work work owkr

--- day22.R
+++ day22.R
@@ -1,4 +1,5 @@
 ### Read input ---------------------
+require(digest)
 inp = readLines('inp/day22_inp.txt')
 inp = c('', inp)
 
@@ -10,66 +11,32 @@ player_1 = player_1[!is.na(player_1)]
 player_2 = as.numeric(players[[2]])
 player_2 = player_2[!is.na(player_2)]
 
-### Star 1 --------------
-part1_player_1 = player_1
-part1_player_2 = player_2
-
-while(length(part1_player_1) > 0 & length(part1_player_2) > 0){
-  p1 = part1_player_1[1]
-  part1_player_1 = part1_player_1[-1]
-  p2 = part1_player_2[1]
-  part1_player_2 = part1_player_2[-1]
-  which_wins = which.max(c(p1,p2))
-  if(which_wins == 1){
-    part1_player_1 = c(part1_player_1, p1, p2)
-  } else {
-    part1_player_2 = c(part1_player_2, p2, p1)
-  }
-  part1_player_1
-  part1_player_2
-}
-
-cat(paste('Solution to part 1 is:', 
-          sum(c(part1_player_1, part1_player_2) * seq(from=(length(c(part1_player_1, part1_player_2))), to=1)), 
-          sep='\n'))
-cat('\n====\n')
-
-### Star 2----------------
-crabs = function(p1_cards, p2_cards){
-  rounds_p1_digest = NULL
-  rounds_p2_digest = NULL
+### Game of crabs ----------------
+crabs = function(p1_cards, p2_cards, SUBGAME=FALSE){
+  digests = list(c('', ''))
   
   # RECURSION CHECK
   while(length(p1_cards) > 0 & length(p2_cards) > 0){
-
-    new_p1_digest = digest(paste(p1_cards,collapse='-'))
-    if(new_p1_digest %in% rounds_p1_digest){
-      return(list(win=1, cards=p1_cards,rec=TRUE))
-    } else {
-      rounds_p1_digest = c(rounds_p1_digest, new_p1_digest)
-    }
     
+    new_p1_digest = digest(paste(p1_cards,collapse='-'))
     new_p2_digest = digest(paste(p2_cards,collapse='-'))
-    if(new_p2_digest %in% rounds_p2_digest){
+    
+    if(any(unlist(lapply(digests, function (x) new_p1_digest == x[1] | new_p2_digest == x[2])))) {
       return(list(win=1, cards=p1_cards,rec=TRUE))
     } else {
-      rounds_p2_digest = c(rounds_p2_digest, new_p2_digest)
+      digests[[length(digests) + 1]] = c(new_p1_digest, new_p2_digest)
     }
     
     p1 = p1_cards[1]
     p1_cards = p1_cards[-1]
     p2 = p2_cards[1]
     p2_cards = p2_cards[-1]
+    
     # SUBGAME?
     if(length(p1_cards) == 0 | length(p2_cards) == 0){
       which_wins = which.max(c(p1,p2))
-    } else  if(length(p1_cards) >= p1 & length(p2_cards) >= p2){
-      sub_game = crabs(p1_cards[1:p1], p2_cards[1:p2])
-      if(sub_game$rec){
-        which_wins = sub_game$win
-      } else {
-        which_wins = sub_game$win
-      }
+    } else  if(length(p1_cards) >= p1 & length(p2_cards) >= p2 & SUBGAME){
+      which_wins = crabs(p1_cards[1:p1], p2_cards[1:p2], SUBGAME)$win
     } else {
       which_wins = which.max(c(p1,p2))
     }
@@ -81,13 +48,24 @@ crabs = function(p1_cards, p2_cards){
     }
   }
   if(length(p1_cards) > 0){
-    return(list(win=1,cards=p1_cards, rec=FALSE))
+    return(list(win=1,cards=p1_cards))
   } else {
-    return(list(win=2,cards=p2_cards, rec=FALSE))
+    return(list(win=2,cards=p2_cards))
   }
 }
 
+
+### Star 1 --------------
 winner = crabs(player_1, player_2)
+
+cat(paste('Solution to part 1 is:',
+          sum(winner$cards * seq(from=length(winner$cards), to=1)),
+          sep='\n'))
+
+cat('\n====\n')
+
+### Star 2----------------
+winner = crabs(player_1, player_2, TRUE)
 
 cat(paste('Solution to part 2 is:', 
           sum(winner$cards * seq(from=length(winner$cards), to=1)), 
